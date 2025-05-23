56c12683c93631032c501c5ecc97ad04b42cf424
Soonoh <soonoh.jung@gmail.com>
2025-05-23 17:08:29 +0900

    fix: alignment of progress and creative value

--- app/helpers/creatives_helper.rb
+++ app/helpers/creatives_helper.rb
@@ -4,9 +4,10 @@ module CreativesHelper
       creatives.map do |creative|
         concat(
           content_tag(:li) do
-            concat(link_to(creative.description, creative, class: "unstyled-link"))
-            # Show progress on the right side
-            concat(content_tag(:span, number_to_percentage(creative.progress * 100, precision: 0), class: "creative-progress", style: "float: right; margin-left: 10px; color: #888; font-size: 0.9em;"))
+            concat(content_tag(:div, class: "creative-row") do
+              concat(link_to(creative.description, creative, class: "unstyled-link"))
+              concat(content_tag(:span, number_to_percentage(creative.progress * 100, precision: 0), class: "creative-progress", style: "margin-left: 10px; color: #888; font-size: 0.9em;"))
+            end)
             if creative.children.any?
               concat(render_creative_tree(creative.children))
             end
