cc66e4a5fcb9a0ea68ed7e385c16bfd14e19da33
Soonoh <soonoh.jung@gmail.com>
2025-05-23 17:23:56 +0900

    feat: top 3 level parents represented as h1, h2, h3

--- app/helpers/creatives_helper.rb
+++ app/helpers/creatives_helper.rb
@@ -1,19 +1,28 @@
 module CreativesHelper
-  def render_creative_tree(creatives)
-    content_tag(:ul) do
+  def render_creative_tree(creatives, level = 1)
+    safe_join(
       creatives.map do |creative|
-        concat(
-          content_tag(:li) do
-            concat(content_tag(:div, class: "creative-row") do
-              concat(link_to(creative.description, creative, class: "unstyled-link"))
-              concat(content_tag(:span, number_to_percentage(creative.progress * 100, precision: 0), class: "creative-progress", style: "margin-left: 10px; color: #888; font-size: 0.9em;"))
-            end)
-            if creative.children.any?
-              concat(render_creative_tree(creative.children))
+        if level <= 3
+          # Render as heading tag for root and second-level only
+          heading_tag = "h#{level}"
+          content_tag(heading_tag, class: "creative-row") do
+            link_to(creative.description, creative, class: "unstyled-link") +
+            content_tag(:span, number_to_percentage(creative.progress * 100, precision: 0), class: "creative-progress", style: "margin-left: 10px; color: #888; font-size: 0.9em;")
+          end +
+          (creative.children.any? ? render_creative_tree(creative.children, level + 1) : "")
+        else
+          # low level creative render as li
+          content_tag(:ul) {
+            content_tag(:li) do
+            content_tag(:div, class: "creative-row") do
+              link_to(creative.description, creative, class: "unstyled-link") +
+              content_tag(:span, number_to_percentage(creative.progress * 100, precision: 0), class: "creative-progress", style: "margin-left: 10px; color: #888; font-size: 0.9em;")
+            end +
+            (creative.children.any? ? render_creative_tree(creative.children, level + 1) : "")
             end
-          end
-        )
+          }
+        end
       end
-    end
+    )
   end
 end
