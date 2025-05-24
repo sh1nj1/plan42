2f610bd627af808095b372afdef10b17841ab780
Soonoh <soonoh.jung@gmail.com>
2025-05-24 14:34:49 +0900

    refactor: remove duplication on render creative row

--- app/helpers/creatives_helper.rb
+++ app/helpers/creatives_helper.rb
@@ -9,25 +9,27 @@ module CreativesHelper
           ondragover: "handleDragOver(event)",
           ondrop: "handleDrop(event)"
         }
+        render_next_block = -> {
+          (creative.children.any? ? render_creative_tree(creative.children, level + 1) : "")
+        }
+        render_row_content = -> {
+          link_to(creative.description, creative, class: "unstyled-link") +
+            content_tag(:span, number_to_percentage(creative.progress * 100, precision: 0), class: "creative-progress", style: "margin-left: 10px; color: #888; font-size: 0.9em;")
+        }
         if level <= 3
-          # Render as heading tag for root and second-level only
           content_tag(:div, class: "creative-tree", **drag_attrs) {
             heading_tag = (creative.children.any? or creative.parent.nil?) ? "h#{level}" : "div"
             content_tag(heading_tag, class: "creative-row indent#{level}") do
-              link_to(creative.description, creative, class: "unstyled-link") +
-              content_tag(:span, number_to_percentage(creative.progress * 100, precision: 0), class: "creative-progress", style: "margin-left: 10px; color: #888; font-size: 0.9em;")
-            end +
-            (creative.children.any? ? render_creative_tree(creative.children, level + 1) : "")
+              render_row_content.call
+            end + render_next_block.call
           }
         else
           # low level creative render as li
           content_tag(:ul, class: "creative-tree", **drag_attrs) {
             content_tag(:li) do
-            content_tag(:div, class: "creative-row") do
-              link_to(creative.description, creative, class: "unstyled-link") +
-              content_tag(:span, number_to_percentage(creative.progress * 100, precision: 0), class: "creative-progress", style: "margin-left: 10px; color: #888; font-size: 0.9em;")
-            end +
-            (creative.children.any? ? render_creative_tree(creative.children, level + 1) : "")
+              content_tag(:div, class: "creative-row") do
+                render_row_content.call
+              end + render_next_block.call
             end
           }
         end
