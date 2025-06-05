24eeaf685c3d1f895896bda767410ddf64aef4bd
Soonoh <soonoh.jung@gmail.com>
2025-06-05 09:51:37 +0900

    chore: add system spec for show sharing users

---
+++ spec/system/creative_share_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Creative 공유 리스트', type: :system do
+  let!(:user) { User.create!(email_address: 'user1@example.com', password: 'password') }
+  let!(:creative) { Creative.create!(description: '테스트', user: user) }
+  let!(:share) { CreativeShare.create!(creative: creative, user: user, permission: 'read') }
+
+  it '공유 리스트가 정상적으로 표시된다' do
+    visit new_session_path
+    fill_in placeholder: I18n.t('users.new.enter_your_email_address'), with: user.email_address
+    fill_in placeholder: I18n.t('users.new.enter_your_password'), with: 'password'
+    click_button I18n.t('app.sign_in')
+    expect(page).not_to have_content(I18n.t('users.sessions.new.try_another_email_or_password'))
+    visit creative_path(creative)
+    click_button I18n.t('creatives.index.share')
+    expect(page).to have_content(I18n.t('creatives.index.shared_with'))
+    expect(page).to have_content('user1@example.com')
+    expect(page).to have_content('user1@example.com - Permission Read')
+  end
+end
