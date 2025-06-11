20d785fcc405fcc99558d4cc525df0b548eadb02
Soonoh <chk0ndanger@gmail.com>
2025-06-11 22:29:18 +0900

    Notify recipients on creative share

--- app/models/creative_share.rb
+++ app/models/creative_share.rb
@@ -14,7 +14,21 @@ class CreativeShare < ApplicationRecord
   validates :permission, presence: true
   validates :user_id, uniqueness: { scope: :creative_id }
 
+  after_create_commit :notify_recipient
+
   private
+
+  def notify_recipient
+    return unless Current.user && user
+    desc = creative.effective_description
+    title = ActionController::Base.helpers.strip_tags(desc)
+    short_title = ActionController::Base.helpers.truncate(title, length: 30)
+    InboxItem.create!(
+      owner: user,
+      message: I18n.t("inbox.creative_shared", user: Current.user.email, short_title: short_title),
+      link: Rails.application.routes.url_helpers.creative_path(creative)
+    )
+  end
 
   def linked_creative_exists?
     Creative.exists?(origin_id: creative.id, user_id: user.id)
---
+++ test/models/creative_share_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class CreativeShareTest < ActiveSupport::TestCase
+  test "creating a share notifies recipient" do
+    creative = creatives(:tshirt)
+    sharer = users(:one)
+    recipient = users(:two)
+
+    Current.session = OpenStruct.new(user: sharer)
+
+    assert_difference("InboxItem.count", 1) do
+      CreativeShare.create!(creative: creative, user: recipient, permission: :read)
+    end
+
+    item = InboxItem.last
+    assert_equal recipient, item.owner
+    assert_includes item.message, sharer.email
+    assert_includes item.message, "T-Shirt"
+    expected_link = Rails.application.routes.url_helpers.creative_path(creative)
+    assert_equal expected_link, item.link
+
+    Current.reset
+  end
+end
