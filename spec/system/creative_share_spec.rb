fdf42a23ba08a72c5733074bf8457ad41def8093
Soonoh <soonoh.jung@gmail.com>
2025-06-16 13:53:08 +0900

    build: fix test for share list popup rspec

--- spec/system/creative_share_spec.rb
+++ spec/system/creative_share_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Creative 공유 리스트', type: :system do
-  let!(:user) { User.create!(email: 'user1@example.com', password: 'password') }
+  let!(:user) { User.create!(email: 'user1@example.com', password: 'password', :email_verified_at => Time.now) }
   let!(:creative) { Creative.create!(description: '테스트', user: user) }
   let!(:share) { CreativeShare.create!(creative: creative, user: user, permission: 'read') }
 
@@ -15,6 +15,6 @@ RSpec.describe 'Creative 공유 리스트', type: :system do
     click_button I18n.t('creatives.index.share')
     expect(page).to have_content(I18n.t('creatives.index.shared_with'))
     expect(page).to have_content('user1@example.com')
-    expect(page).to have_content('user1@example.com - Permission Read')
+    expect(page).to have_content("user1@example.com\nRead")
   end
 end
