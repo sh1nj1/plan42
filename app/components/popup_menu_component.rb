80f132574a51964d0a180e9e3f9911b7bdc75dc1
Soonoh <chk0ndanger@gmail.com>
2025-06-18 13:38:36 +0900

    Move delete action to mobile popup

--- app/components/popup_menu_component.rb
+++ app/components/popup_menu_component.rb
@@ -1,10 +1,11 @@
 class PopupMenuComponent < ViewComponent::Base
-  def initialize(button_content:, button_classes: "", menu_id: nil, align: :left)
+  def initialize(button_content:, button_classes: "", menu_id: nil, align: :left, button_id: nil)
     @button_content = button_content
     @button_classes = button_classes
     @menu_id = menu_id || "popup-menu-#{SecureRandom.hex(4)}"
     @align = align
+    @button_id = button_id
   end
 
-  attr_reader :button_content, :button_classes, :menu_id, :align
+  attr_reader :button_content, :button_classes, :menu_id, :align, :button_id
 end
