0d95ac5254e89b5eb6ccffe9eb225d81d6746efa
Soonoh <chk0ndanger@gmail.com>
2025-06-20 18:04:21 +0900

    feat: show comment count with badge component

--- app/components/inbox/badge_component.rb
+++ app/components/inbox/badge_component.rb
@@ -1,11 +1,15 @@
 module Inbox
   class BadgeComponent < ViewComponent::Base
-    def initialize(user:)
+    attr_reader :badge_id
+
+    def initialize(user: nil, count: nil, badge_id: "inbox-badge")
       @user = user
+      @count = count
+      @badge_id = badge_id
     end
 
     def count
-      InboxItem.where(owner: @user, state: "new").count
+      @count || InboxItem.where(owner: @user, state: "new").count
     end
   end
 end
