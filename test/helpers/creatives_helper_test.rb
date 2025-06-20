6b66225a733dc52592bed414ec4f5d2bd1698683
Soonoh <chk0ndanger@gmail.com>
2025-06-20 10:52:38 +0900

    Add markdown helper test

--- test/helpers/creatives_helper_test.rb
+++ test/helpers/creatives_helper_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class CreativesHelperTest < ActionView::TestCase
+  include CreativesHelper
   test "markdown_links_to_html converts markdown link to HTML" do
     input = "Check [link](https://example.com)"
     expected = "Check <a href=\"https://example.com\">link</a>"
@@ -11,5 +12,12 @@ class CreativesHelperTest < ActionView::TestCase
     input = "See <a href=\"https://example.com\">example</a> for details"
     expected = "See [example](https://example.com) for details"
     assert_equal expected, html_links_to_markdown(input)
+  end
+
+  test "markdown list items are single line" do
+    user = users(:one)
+    creative = Creative.create!(user: user, description: "<div>Item</div>\n")
+    markdown = render_creative_tree_markdown([ creative ], 5)
+    assert_equal "* Item\n", markdown
   end
 end
