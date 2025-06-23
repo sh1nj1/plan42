7f2b2807329760fff384e8fe211c436712f33dd5
Soonoh <chk0ndanger@gmail.com>
2025-06-23 09:50:28 +0900

    Refactor comment notification logic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,6 +12,28 @@ class Comment < ApplicationRecord
 
   private
 
+  def create_inbox_item(owner, message)
+    InboxItem.create!(
+      owner: owner,
+      message: message,
+      link: Rails.application.routes.url_helpers.creative_comment_path(creative, self)
+    )
+  end
+
+  def mentioned_emails
+    return [] unless content
+    content.scan(/@([\w.\-+]+@[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,})/)
+           .flatten
+           .map(&:downcase)
+           .uniq
+  end
+
+  def mentioned_users
+    return [] unless user
+    emails = mentioned_emails - [ user.email.downcase ]
+    User.where(email: emails)
+  end
+
   def broadcast_create
     broadcast_prepend_later_to([ creative, :comments ], target: "comments_list")
   end
@@ -26,24 +48,17 @@ class Comment < ApplicationRecord
 
   def notify_creative_owner
     return unless creative.user && user && creative.user != user
-
-    InboxItem.create!(
-      owner: creative.user,
-      message: I18n.t("inbox.comment_added", user: user.email, comment: content),
-      link: Rails.application.routes.url_helpers.creative_comment_path(creative, self)
+    create_inbox_item(
+      creative.user,
+      I18n.t("inbox.comment_added", user: user.email, comment: content)
     )
   end
 
   def notify_mentions
-    return unless user
-    emails = content.scan(/@([\w.\-+]+@[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,})/).flatten
-    emails.each do |email|
-      mentioned = User.find_by(email: email.downcase)
-      next unless mentioned && mentioned != user
-      InboxItem.create!(
-        owner: mentioned,
-        message: I18n.t("inbox.user_mentioned", user: user.email, comment: content),
-        link: Rails.application.routes.url_helpers.creative_comment_path(creative, self)
+    mentioned_users.each do |mentioned|
+      create_inbox_item(
+        mentioned,
+        I18n.t("inbox.user_mentioned", user: user.email, comment: content)
       )
     end
   end
