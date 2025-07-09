9e710b1852260acbda041917552e5adf1858fc24
Soonoh <soonoh.jung@gmail.com>
2025-07-09 14:39:30 +0900

    Fix inbox panel bug on narrow screens (#144)
    * Fix inbox panel responsive bug and adjust tests
    
    * Make chromedriver path configurable and add CSS spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,12 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
 require 'rspec/rails'
+if defined?(Selenium::WebDriver::Chrome)
+  chromedriver_path = ENV['CHROMEDRIVER_PATH'] || `which chromedriver`.strip
+  if File.exist?(chromedriver_path)
+    Selenium::WebDriver::Chrome::Service.driver_path = chromedriver_path
+  end
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -39,6 +45,10 @@ RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
+
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
