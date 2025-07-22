261dcd51f349ba174f64e2e171fbb04e146721b0
Soonoh <soonoh.jung@gmail.com>
2025-07-22 21:38:42 +0900

    refactor: fix formatting in fcm.rb

--- config/initializers/fcm.rb
+++ config/initializers/fcm.rb
@@ -10,13 +10,13 @@ if project_id.present?
     audience: audience,
     subject_token_type: "urn:ietf:params:aws:token-type:aws4_request",
     token_url: "https://sts.googleapis.com/v1/token",
-    scopes: [Google::Apis::FcmV1::AUTH_FIREBASE_MESSAGING],
+    scopes: [ Google::Apis::FcmV1::AUTH_FIREBASE_MESSAGING ],
     credential_source: {
       environment_id: "aws1",
       region_url: "http://169.254.169.254/latest/meta-data/placement/region",
       url: "http://169.254.169.254/latest/meta-data/iam/security-credentials/",
       regional_cred_verification_url: "https://sts.amazonaws.com?Action=GetCallerIdentity&Version=2011-06-15",
-      imds_v2_session_token_url: 'http://169.254.169.254/latest/api/token'
+      imds_v2_session_token_url: "http://169.254.169.254/latest/api/token"
     }
   )
 
