a05f324eff2544f6627a2cdce488bc399269ec85
Soonoh <chk0ndanger@gmail.com>
2025-08-22 17:55:48 +0900

    Add NO_ACCESS permission

---
+++ db/migrate/20250620000000_add_no_access_permission.rb
@@ -0,0 +1,25 @@
+class AddNoAccessPermission < ActiveRecord::Migration[6.1]
+  def up
+    execute <<~SQL
+      UPDATE creative_shares
+      SET permission = permission + 1
+    SQL
+    execute <<~SQL
+      UPDATE invitations
+      SET permission = permission + 1
+      WHERE permission IS NOT NULL
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      UPDATE creative_shares
+      SET permission = permission - 1
+    SQL
+    execute <<~SQL
+      UPDATE invitations
+      SET permission = permission - 1
+      WHERE permission IS NOT NULL
+    SQL
+  end
+end
