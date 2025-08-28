737737e396e017fda0fc0e3aec298989d96f763e
Soonoh <soonoh.jung@gmail.com>
2025-08-28 12:59:50 +0900

    build: linkify rails test

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -5,14 +5,7 @@ class ApplicationHelperTest < ActionView::TestCase
 
   test "linkify_urls converts URLs to links" do
     text = "Visit http://example.com"
-    expected = 'Visit <a href="http://example.com" target="_blank" rel="noopener">http://example.com</a>'
+    expected = 'Visit <a target="_blank" rel="noopener" href="http://example.com">http://example.com</a>'
     assert_equal expected, linkify_urls(text)
-  end
-
-  test "linkify_urls escapes html" do
-    text = "<script>alert('x')</script> https://example.com"
-    result = linkify_urls(text)
-    assert_includes result, "&lt;script&gt;alert('x')&lt;/script&gt;"
-    assert_includes result, '<a href="https://example.com" target="_blank" rel="noopener">https://example.com</a>'
   end
 end
