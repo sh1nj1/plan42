df9cf541cad85b6ce8236878b013065707d120a5
Soonoh <chk0ndanger@gmail.com>
2025-09-15 11:18:49 +0900

    Replace importmap with jsbundling-rails

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,12 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "./application"
+
+// Eager load all controllers within this directory and register them
+const controllers = import.meta.glob("./**/*_controller.js", { eager: true })
+for (const path in controllers) {
+  const identifier = path
+    .replace("./", "")
+    .replace(/_controller\.js$/, "")
+    .replace(/\//g, "--")
+    .replace(/_/g, "-")
+  application.register(identifier, controllers[path].default)
+}
