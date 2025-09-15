2bf6dab141bb8bd89e1126fd000ecd3694d31247
Soonoh <soonoh.jung@gmail.com>
2025-09-15 20:49:58 +0900

    refactor: remove unused code

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,7 @@
 import { application } from "./application"
 
 import CommentController from "./comment_controller"
-import HelloController from "./hello_controller"
 import ShareInviteController from "./share_invite_controller"
 
 application.register("comment", CommentController)
-application.register("hello", HelloController)
 application.register("share-invite", ShareInviteController)
