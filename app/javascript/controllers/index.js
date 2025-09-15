7fa57e967865b24c2508b14ac1c7899e18eaf68f
Soonoh <chk0ndanger@gmail.com>
2025-09-15 13:05:40 +0900

    Fix Stimulus controller registration without import.meta

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,12 +1,9 @@
 import { application } from "./application"
 
-// Eager load all controllers within this directory and register them
-const controllers = import.meta.glob("./**/*_controller.js", { eager: true })
-for (const path in controllers) {
-  const identifier = path
-    .replace("./", "")
-    .replace(/_controller\.js$/, "")
-    .replace(/\//g, "--")
-    .replace(/_/g, "-")
-  application.register(identifier, controllers[path].default)
-}
+import CommentController from "./comment_controller"
+import HelloController from "./hello_controller"
+import ShareInviteController from "./share_invite_controller"
+
+application.register("comment", CommentController)
+application.register("hello", HelloController)
+application.register("share-invite", ShareInviteController)
