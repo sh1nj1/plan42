a582f67c19b3de78d086b99b678391667d11214b
Soonoh <chk0ndanger@gmail.com>
2025-09-17 17:28:23 +0900

    Harden link preview fetching against SSRF

--- app/services/link_preview_fetcher.rb
+++ app/services/link_preview_fetcher.rb
@@ -1,17 +1,32 @@
 require "open-uri"
 require "nokogiri"
 require "uri"
+require "ipaddr"
+require "resolv"
 
 class LinkPreviewFetcher
   USER_AGENT = "Plan42LinkPreview/1.0".freeze
   OPEN_TIMEOUT = 5
   READ_TIMEOUT = 5
   MAX_BYTES = 200_000
+  MAX_REDIRECTS = 3
   HTML_CONTENT_TYPES = [ "text/html", "application/xhtml+xml" ].freeze
   REQUEST_OPTIONS = {
     "User-Agent" => USER_AGENT,
     "Accept" => "text/html,application/xhtml+xml"
   }.freeze
+  DISALLOWED_IPV4_RANGES = [
+    IPAddr.new("0.0.0.0/8"),
+    IPAddr.new("100.64.0.0/10"),
+    IPAddr.new("192.0.0.0/24"),
+    IPAddr.new("198.18.0.0/15"),
+    IPAddr.new("224.0.0.0/4"),
+    IPAddr.new("240.0.0.0/4")
+  ].freeze
+  DISALLOWED_IPV6_RANGES = [
+    IPAddr.new("::/128"),
+    IPAddr.new("ff00::/8")
+  ].freeze
 
   def self.fetch(url)
     new(url).fetch
@@ -24,9 +39,10 @@ class LinkPreviewFetcher
   end
 
   def fetch
-    return {} unless valid_http_url?
+    uri = safe_http_uri
+    return {} unless uri
 
-    html, base_uri = read_html
+    html, base_uri = read_html(uri)
     return {} if html.blank?
 
     document = Nokogiri::HTML(html)
@@ -38,29 +54,94 @@ class LinkPreviewFetcher
 
   private
 
-  def valid_http_url?
-    uri = URI.parse(@url)
-    %w[http https].include?(uri.scheme)
-  rescue URI::InvalidURIError
-    false
-  end
-
-  def read_html
+  def read_html(uri, redirect_limit = MAX_REDIRECTS)
     html = nil
     base_uri = nil
-    options = REQUEST_OPTIONS.merge(read_timeout: READ_TIMEOUT, open_timeout: OPEN_TIMEOUT)
-    @io_opener.open(@url, options) do |io|
+    options = REQUEST_OPTIONS.merge(read_timeout: READ_TIMEOUT, open_timeout: OPEN_TIMEOUT, redirect: false)
+    @io_opener.open(uri.to_s, options) do |io|
       content_type = io.respond_to?(:content_type) ? io.content_type : nil
       if content_type && HTML_CONTENT_TYPES.none? { |type| content_type.include?(type) }
         return [ nil, nil ]
       end
-      base_uri = io.respond_to?(:base_uri) ? io.base_uri : URI.parse(@url)
+      base_uri = io.respond_to?(:base_uri) ? io.base_uri : uri
       html = io.read(MAX_BYTES)
     end
     [ html, base_uri ]
+  rescue OpenURI::HTTPRedirect => e
+    return [ nil, nil ] if redirect_limit <= 0
+
+    redirected_uri = safe_redirect_uri(uri, e.uri)
+    return [ nil, nil ] unless redirected_uri
+
+    read_html(redirected_uri, redirect_limit - 1)
   rescue OpenURI::HTTPError, SocketError, IOError, SystemCallError, URI::InvalidURIError => e
     @logger&.info("Link preview fetch skipped for #{@url}: #{e.class} #{e.message}")
     [ nil, nil ]
+  end
+
+  def safe_http_uri
+    uri = parse_http_uri(@url)
+    return unless uri
+    return unless allowed_destination?(uri)
+
+    uri
+  end
+
+  def safe_redirect_uri(current_uri, redirected)
+    new_uri = normalize_redirect_uri(current_uri, redirected)
+    return unless new_uri
+    return unless allowed_destination?(new_uri)
+
+    new_uri
+  end
+
+  def normalize_redirect_uri(current_uri, redirected)
+    target_uri = redirected.is_a?(URI) ? redirected : URI.parse(redirected.to_s)
+    target_uri = current_uri.merge(target_uri) if target_uri.relative?
+    return unless %w[http https].include?(target_uri.scheme)
+
+    target_uri
+  rescue URI::InvalidURIError
+    nil
+  end
+
+  def parse_http_uri(url)
+    uri = URI.parse(url)
+    return unless %w[http https].include?(uri.scheme)
+    return unless uri.hostname && !uri.hostname.empty?
+
+    uri
+  rescue URI::InvalidURIError
+    nil
+  end
+
+  def allowed_destination?(uri)
+    host = uri.hostname
+    return false if host.nil? || host.empty?
+
+    addresses = resolve_addresses(host)
+    return false if addresses.empty?
+    return false if addresses.any? { |address| unsafe_ip?(address) }
+
+    true
+  end
+
+  def resolve_addresses(host)
+    Resolv.getaddresses(host).uniq
+  rescue Resolv::ResolvError, SocketError, ArgumentError
+    []
+  end
+
+  def unsafe_ip?(address)
+    ip = IPAddr.new(address)
+    ip = ip.native if ip.ipv4_mapped?
+
+    return true if ip.loopback? || ip.link_local? || ip.private?
+
+    ranges = ip.ipv4? ? DISALLOWED_IPV4_RANGES : DISALLOWED_IPV6_RANGES
+    ranges.any? { |range| range.include?(ip) }
+  rescue IPAddr::InvalidAddressError
+    true
   end
 
   def build_metadata(document, base_uri)
