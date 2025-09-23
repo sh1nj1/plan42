6f8eb4bda71a526bc9a237a447ff3fb054eee0de
Soonoh <soonoh.jung@gmail.com>
2025-09-23 20:34:23 +0900

    refactor: to minitest from assets, mailers rspec

---
+++ test/assets/inbox_panel_css_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class InboxPanelCssTest < ActiveSupport::TestCase
+  test 'defines open state for small screens' do
+    css = File.read(Rails.root.join('app/assets/stylesheets/application.css'))
+    assert_includes css, '@media (max-width: 360px)'
+
+    pattern = /#inbox-panel\.slide-panel\.open\s*\{[^\}]*right:\s*0;[^\}]*\}/
+    assert_match pattern, css
+  end
+end
---
+++ test/mailers/example_domain_interceptor_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ExampleDomainInterceptorTest < ActiveSupport::TestCase
+  setup { ActionMailer::Base.deliveries.clear }
+  teardown { ActionMailer::Base.deliveries.clear }
+
+  test 'does not deliver emails to example.com addresses' do
+    user = User.create!(email: 'blocked@example.com', password: 'secret', name: 'Blocked')
+
+    UserMailer.email_verification(user).deliver_now
+
+    assert_empty ActionMailer::Base.deliveries
+  end
+
+  test 'delivers emails to other domains' do
+    user = User.create!(email: 'allowed@domain.com', password: 'secret', name: 'Allowed')
+
+    UserMailer.email_verification(user).deliver_now
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+  end
+end
