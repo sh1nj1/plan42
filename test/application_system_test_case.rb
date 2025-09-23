b009bf4e344fa1d199a48b5dc88d4de3ac50050a
Soonoh <soonoh.jung@gmail.com>
2025-09-23 23:19:22 +0900

    build: use custom headless chrome for system test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,23 @@
 require "test_helper"
 require_relative "support/system_helpers"
+require "tmpdir"
+
+Capybara.register_driver :custom_headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument "--headless"
+  options.add_argument "--disable-gpu"
+  options.add_argument "--no-sandbox"
+  options.add_argument "--disable-dev-shm-usage"
+  options.add_argument "--window-size=1920,1080"
+  options.add_argument "--user-data-dir=#{Dir.mktmpdir("chrome-profile-#{Process.pid}-")}"
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include SystemHelpers
 
-  driven_by :selenium, using: :chrome, screen_size: [ 1920, 1080 ]
+  # for local development test with chrome
+  # driven_by :selenium, using: :chrome, screen_size: [ 1920, 1080 ]
+  driven_by :custom_headless_chrome
 end
