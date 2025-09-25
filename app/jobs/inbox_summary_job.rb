dafa07db7e2f3b1decbc64b3f509556fc588f0a8
Soonoh <chk0ndanger@gmail.com>
2025-09-25 16:13:23 +0900

    Skip inbox summary email when notifications disabled

--- app/jobs/inbox_summary_job.rb
+++ app/jobs/inbox_summary_job.rb
@@ -4,6 +4,11 @@ class InboxSummaryJob < ApplicationJob
   def perform
     Rails.logger.info("Running InboxSummaryJob")
     User.find_each do |user|
+      if user.notifications_enabled == false
+        Rails.logger.info("Skipping inbox summary for #{user.email} because notifications are disabled")
+        next
+      end
+
       items = InboxItem.where(owner: user, state: "new").order(created_at: :desc).limit(10)
       if items.empty?
         Rails.logger.info("No new inbox items for #{user.email}")
