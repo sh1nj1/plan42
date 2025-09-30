f910b4def304041d71a2b1a1322c4d539c104874
Soonoh <soonoh.jung@gmail.com>
2025-09-30 23:16:01 +0900

    Fix unconvert to move entire creative tree (#423)
    * Fix unconvert to move entire creative tree
    
    * Fix unconvert subtree deletion order
    
    * build: fix test

--- test/controllers/creatives_controller_test.rb
+++ test/controllers/creatives_controller_test.rb
@@ -5,14 +5,14 @@ class CreativesControllerTest < ActionDispatch::IntegrationTest
     sign_in_as(users(:one), password: "password")
   end
 
-  test "unconvert moves children into parent comment" do
+  test "unconvert moves creative tree into parent comment" do
     creative = creatives(:unconvert_target)
     parent = creative.parent
-    children = creative.children.order(:sequence).to_a
-    expected_markdown = ApplicationController.helpers.render_creative_tree_markdown(children)
+    grandchild = creatives(:unconvert_grandchild)
+    expected_markdown = ApplicationController.helpers.render_creative_tree_markdown([ creative ])
 
     assert_difference -> { parent.comments.count }, 1 do
-      assert_difference -> { creative.children.count }, -children.count do
+      assert_difference -> { parent.children.count }, -1 do
         post unconvert_creative_path(creative), headers: { "ACCEPT" => "application/json" }
       end
     end
@@ -21,8 +21,8 @@ class CreativesControllerTest < ActionDispatch::IntegrationTest
     parent.reload
     comment = parent.comments.order(:created_at).last
     assert_equal expected_markdown, comment.content
-    creative.reload
-    assert_equal 0, creative.children.count
+    assert_raises(ActiveRecord::RecordNotFound) { creative.reload }
+    assert_raises(ActiveRecord::RecordNotFound) { grandchild.reload }
   end
 
   test "unconvert without children returns error" do
