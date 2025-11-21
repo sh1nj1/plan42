3fd96f5d282045a51c3230294f345d034d41d882
Soonoh <chk0ndanger@gmail.com>
2025-11-21 16:37:16 +0900

    Handle shared_by cleanup when users are destroyed

--- db/migrate/20251002000000_add_shared_by_to_creative_shares.rb
+++ db/migrate/20251002000000_add_shared_by_to_creative_shares.rb
@@ -1,6 +1,6 @@
 class AddSharedByToCreativeShares < ActiveRecord::Migration[7.1]
   def change
-    add_reference :creative_shares, :shared_by, foreign_key: { to_table: :users }
+    add_reference :creative_shares, :shared_by, foreign_key: { to_table: :users, on_delete: :nullify }
 
     reversible do |dir|
       dir.up do
