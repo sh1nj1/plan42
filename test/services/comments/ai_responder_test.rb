c41f3a60ec497c77885824082dd74a47a25e6055
Soonoh <chk0ndanger@gmail.com>
2025-12-02 02:04:06 -0800

    Support Liquid templates for AI prompts

--- test/services/comments/ai_responder_test.rb
+++ test/services/comments/ai_responder_test.rb
@@ -64,5 +64,45 @@ module Comments
       reply = creative.comments.where(user: ai_user).order(:id).last
       assert_equal "pong", reply.content
     end
+
+    test "renders liquid variables in system prompt" do
+      creative = creatives(:tshirt)
+      commenter = users(:one)
+      ai_user = User.create!(
+        name: "AIBot",
+        email: "aibot@example.com",
+        password: "password",
+        llm_vendor: "google",
+        llm_model: "gemini-2.5-flash",
+        system_prompt: "Assist {{ai_user.name}} on {{creative.description}} for {{comment.user_name}} about {{payload}}",
+        llm_api_key: "dummy-key",
+        email_verified_at: Time.current,
+        searchable: true
+      )
+
+      comment = creative.comments.create!(content: "@AIBot: ping please", user: commenter)
+
+      captured_prompt = nil
+      fake_client = Class.new do
+        attr_reader :system_prompt
+
+        def initialize(**args)
+          @system_prompt = args[:system_prompt]
+        end
+
+        def chat(_messages, tools: [])
+          yield "pong" if block_given?
+          "pong"
+        end
+      end
+
+      AiClient.stub(:new, ->(**args) { instance = fake_client.new(**args); captured_prompt = instance.system_prompt; instance }) do
+        Comments::AiResponder.new(comment: comment, creative: creative).call
+      end
+
+      assert_equal "Assist AIBot on T-Shirt for One about ping please", captured_prompt
+      reply = creative.comments.where(user: ai_user).order(:id).last
+      assert_equal "pong", reply.content
+    end
   end
 end
