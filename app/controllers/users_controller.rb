fdce7be10ab66c37ae84f6ae7884938f14f14371
Soonoh Jung <soonoh.jung@gmail.com>
2025-12-05 12:01:16 +0900

    chore: add comment for routing_expresion when new_ai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
       searchable: searchable,
       email_verified_at: Time.current, # Auto-verified
       created_by_id: Current.user.id,
-      routing_expression: params[:routing_expression]
+      routing_expression: params[:routing_expression] # allow empty for not routing for now
     )
 
     if @user.save
