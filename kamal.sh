74b0a828d77a804eec30878397b3f174ddc8ef6c
Soonoh Jung <soonoh.jung@gmail.com>
2025-12-19 09:06:53 +0900

    build: use RAILS_ENV in kamal.sh

--- kamal.sh
+++ kamal.sh
@@ -1,2 +1,2 @@
 # `bundle binstubs dotenv` creates ./bin/dotenv
-./bin/dotenv --overwrite -f ".env.production" ./bin/kamal $@
+./bin/dotenv --overwrite -f ".env.${RAILS_ENV:-production}" ./bin/kamal $@
