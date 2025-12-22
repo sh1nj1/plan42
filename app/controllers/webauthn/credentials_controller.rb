25ebc3ea0839005ff50ef3ded7b4f678beb5d9df
Soonoh <soonoh.jung@gmail.com>
2025-12-23 00:51:53 +0900

    fix: Use i18n for passkey deletion message

--- app/controllers/webauthn/credentials_controller.rb
+++ app/controllers/webauthn/credentials_controller.rb
@@ -3,7 +3,7 @@ module Webauthn
     def destroy
       credential = Current.user.webauthn_credentials.find(params[:id])
       credential.destroy
-      redirect_back fallback_location: root_path, notice: "Passkey deleted."
+      redirect_back fallback_location: root_path, notice: I18n.t("users.webauthn.deleted")
     end
   end
 end
