48fbbe848c0a900d01a218fb6312399e18ddb8b9
Soonoh <soonoh.jung@gmail.com>
2025-12-23 00:18:37 +0900

    fix: Handle test environment in WebAuthn initializer

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,8 +1,12 @@
 WebAuthn.configure do |config|
-  url_options = Rails.configuration.action_controller.default_url_options
-  origin = "#{url_options[:protocol]}://#{url_options[:host]}"
-  origin << ":#{url_options[:port]}" if url_options[:port]
-  config.allowed_origins = [ origin ]
+  if Rails.env.test?
+    config.allowed_origins = [ "http://www.example.com" ]
+  else
+    url_options = Rails.configuration.action_controller.default_url_options
+    origin = "#{url_options[:protocol]}://#{url_options[:host]}"
+    origin << ":#{url_options[:port]}" if url_options[:port]
+    config.allowed_origins = [ origin ]
+  end
   config.rp_name = I18n.t("app.name")
   config.credential_options_timeout = 120000
 end
