f6d4b3b188d234fab1fc84bf2d98b4feed643de2
Soonoh <soonoh.jung@gmail.com>
2026-01-21 02:18:26 +0900

    perf: batch cleanup job deletes and tune authz polling interval
    - Cleanup job now deletes in batches of 1000 to reduce table locks
    - Authz worker polling interval increased from 0.05s to 0.1s (env-configurable
      via AUTHZ_POLLING_INTERVAL) to reduce CPU churn

--- app/jobs/permission_cache_cleanup_job.rb
+++ app/jobs/permission_cache_cleanup_job.rb
@@ -1,27 +1,36 @@
 class PermissionCacheCleanupJob < ApplicationJob
   queue_as :default
 
+  BATCH_SIZE = 1000
+
   def perform
-    # Remove cache rows for deleted creatives
-    orphaned_by_creative = CreativeSharesCache
-      .where.not(creative_id: Creative.select(:id))
-      .delete_all
+    orphaned_by_creative = delete_in_batches(
+      CreativeSharesCache.where.not(creative_id: Creative.select(:id))
+    )
 
-    # Remove cache rows for deleted users (exclude NULL for public shares)
-    orphaned_by_user = CreativeSharesCache
-      .where.not(user_id: nil)
-      .where.not(user_id: User.select(:id))
-      .delete_all
+    orphaned_by_user = delete_in_batches(
+      CreativeSharesCache.where.not(user_id: nil).where.not(user_id: User.select(:id))
+    )
 
-    # Remove cache rows for deleted shares (source_share_id no longer exists)
-    orphaned_by_share = CreativeSharesCache
-      .where.not(source_share_id: nil)
-      .where.not(source_share_id: CreativeShare.select(:id))
-      .delete_all
+    orphaned_by_share = delete_in_batches(
+      CreativeSharesCache.where.not(source_share_id: nil).where.not(source_share_id: CreativeShare.select(:id))
+    )
 
     Rails.logger.info(
       "[PermissionCacheCleanupJob] Cleaned up orphaned cache rows: " \
       "creative=#{orphaned_by_creative}, user=#{orphaned_by_user}, share=#{orphaned_by_share}"
     )
+  end
+
+  private
+
+  def delete_in_batches(scope)
+    total_deleted = 0
+    loop do
+      deleted = scope.limit(BATCH_SIZE).delete_all
+      total_deleted += deleted
+      break if deleted < BATCH_SIZE
+    end
+    total_deleted
   end
 end
