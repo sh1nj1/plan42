f1682101808a345eb794b0a8385c29a5838fa41a
Soonoh Jung <soonoh.jung@gmail.com>
2026-01-20 16:56:30 +0900

    build: use PORT for development app port

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Collavre
     unless Rails.env.test?
       url_options = {
         host: ENV.fetch("DEFAULT_URL_HOST", "localhost"),
-        port: ENV.fetch("DEFAULT_URL_PORT", "3000"),
+        port: ENV.fetch("PORT") { ENV.fetch("DEFAULT_URL_PORT", "3000") },
         protocol: ENV.fetch("DEFAULT_URL_PROTOCOL", "http")
       }
 
