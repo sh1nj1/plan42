167f000fbd8a31296df08d2d9cbaedc37b775adb
Soonoh Jung <soonoh.jung@rowan.kr>
2026-01-29 10:00:35 +0900

    build: fix rake test:system set test env

--- config/boot.rb
+++ config/boot.rb
@@ -7,7 +7,7 @@ require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 # This ensures RAILS_MASTER_KEY is available for credentials decryption
 require "dotenv"
 
-# Set RAILS_ENV to test if running test command before dotenv loads
-ENV["RAILS_ENV"] ||= "test" if ARGV.first == "test"
+# Handles: rails test, rake test, rake test:system, rake test:all, etc.
+ENV["RAILS_ENV"] ||= "test" if ARGV.any? { |arg| arg == "test" || arg.start_with?("test:") }
 
 Dotenv.overload(".env.#{ENV.fetch('RAILS_ENV', 'development')}")
