b1896e2df76fc45c2137dcc8639695dfa0ef46c0
Soonoh Jung <soonoh.jung@gmail.com>
2026-01-30 13:13:49 +0900

    fix: formatting

--- engines/collavre/app/components/collavre/autocomplete_popup_component.rb
+++ engines/collavre/app/components/collavre/autocomplete_popup_component.rb
@@ -8,11 +8,11 @@ module Collavre
     attr_reader :menu_id, :extra_classes
 
     def css_classes
-      ["common-popup", extra_classes].compact.join(" ")
+      [ "common-popup", extra_classes ].compact.join(" ")
     end
 
     def list_classes
-      ["common-popup-list", extra_classes ? "#{extra_classes.split.first}-results" : nil].compact.join(" ")
+      [ "common-popup-list", extra_classes ? "#{extra_classes.split.first}-results" : nil ].compact.join(" ")
     end
   end
 end
