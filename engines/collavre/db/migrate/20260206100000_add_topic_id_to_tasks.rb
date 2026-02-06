4bc3ee3d27d4de82136738b21bfae6273e69cd40
Soonoh <soonoh.jung@gmail.com>
2026-02-06 15:13:27 +0900

    style: fix SpaceInsideArrayLiteralBrackets in migration

--- engines/collavre/db/migrate/20260206100000_add_topic_id_to_tasks.rb
+++ engines/collavre/db/migrate/20260206100000_add_topic_id_to_tasks.rb
@@ -1,6 +1,6 @@
 class AddTopicIdToTasks < ActiveRecord::Migration[8.0]
   def change
     add_column :tasks, :topic_id, :integer, null: true
-    add_index :tasks, [:topic_id, :status]
+    add_index :tasks, [ :topic_id, :status ]
   end
 end
