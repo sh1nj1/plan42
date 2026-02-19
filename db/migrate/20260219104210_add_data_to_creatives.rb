899cefdb4c90f53948e06ad4d6868a82c43cd9c4
Soonoh <soonoh.jung@gmail.com>
2026-02-19 20:14:25 +0900

    fix: use json type for SQLite, jsonb for PostgreSQL in data column

--- db/migrate/20260219104210_add_data_to_creatives.rb
+++ db/migrate/20260219104210_add_data_to_creatives.rb
@@ -1,6 +1,10 @@
 class AddDataToCreatives < ActiveRecord::Migration[8.1]
   def change
-    add_column :creatives, :data, :jsonb, default: {}, null: false
-    add_index :creatives, :data, using: :gin
+    if connection.adapter_name == "PostgreSQL"
+      add_column :creatives, :data, :jsonb, default: {}, null: false
+      add_index :creatives, :data, using: :gin
+    else
+      add_column :creatives, :data, :json, default: {}, null: false
+    end
   end
 end
