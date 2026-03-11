25a45d11b98fdfe7d4658e0b9de8063bec407af8
Soonoh <soonoh.jung@gmail.com>
2026-03-11 23:43:19 +0900

    fix: cascade delete quoting comments when quoted comment is destroyed
    Change quoting_comments dependent strategy from :nullify to :destroy.
    When a comment is deleted, all comments that quote it are now also
    deleted (including nested quoting chains). The broadcast_destroy
    callback ensures removed comments are also cleared from the UI
    via Turbo Streams.
    
    Closes #9768

--- engines/collavre/app/models/collavre/comment.rb
+++ engines/collavre/app/models/collavre/comment.rb
@@ -26,7 +26,7 @@ module Collavre
     has_many :comment_reactions, class_name: "Collavre::CommentReaction", dependent: :destroy
     has_many :comment_versions, class_name: "Collavre::CommentVersion", dependent: :destroy
     has_many :inbox_items, class_name: "Collavre::InboxItem", dependent: :nullify
-    has_many :quoting_comments, class_name: "Collavre::Comment", foreign_key: :quoted_comment_id, dependent: :nullify
+    has_many :quoting_comments, class_name: "Collavre::Comment", foreign_key: :quoted_comment_id, dependent: :destroy
     belongs_to :selected_version, class_name: "Collavre::CommentVersion", optional: true
 
     has_many_attached :images, dependent: :purge_later
