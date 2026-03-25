140422f2274ffcc8643f2880faf20d11b7cf732a
Soonoh <soonoh.jung@gmail.com>
2026-03-25 23:09:17 +0900

    fix: restore Collavre namespace for CreativesChannel (Zeitwerk autoload)

--- engines/collavre/app/channels/collavre/creatives_channel.rb
+++ engines/collavre/app/channels/collavre/creatives_channel.rb
@@ -1,55 +1,57 @@
-class CreativesChannel < ApplicationCable::Channel
-  def subscribed
-    return reject unless params[:root_id].present? && current_user
+module Collavre
+  class CreativesChannel < ApplicationCable::Channel
+    def subscribed
+      return reject unless params[:root_id].present? && current_user
 
-    @root = Collavre::Creative.find_by(id: params[:root_id])&.effective_origin
-    return reject unless @root
-    return reject unless @root.has_permission?(current_user, :read)
+      @root = Creative.find_by(id: params[:root_id])&.effective_origin
+      return reject unless @root
+      return reject unless @root.has_permission?(current_user, :read)
 
-    Rails.logger.info "[CreativesChannel] User #{current_user.email} subscribed to root #{@root.id}"
-    stream_for @root
-    Collavre::CreativePresenceStore.add(@root.id, current_user.id)
-    broadcast_presence
-  end
-
-  def unsubscribed
-    if @root && current_user
-      Collavre::CreativePresenceStore.remove(@root.id, current_user.id)
+      Rails.logger.info "[CreativesChannel] User #{current_user.email} subscribed to root #{@root.id}"
+      stream_for @root
+      CreativePresenceStore.add(@root.id, current_user.id)
       broadcast_presence
     end
-  end
 
-  def editing(data)
-    return unless @root && current_user
+    def unsubscribed
+      if @root && current_user
+        CreativePresenceStore.remove(@root.id, current_user.id)
+        broadcast_presence
+      end
+    end
 
-    creative_id = data["creative_id"].to_i
-    Rails.logger.info "[CreativesChannel] User #{current_user.email} editing creative##{creative_id}"
-    CreativesChannel.broadcast_to(@root, {
-      editing: {
-        creative_id: creative_id,
-        user_id: current_user.id,
-        user_name: current_user.display_name
-      }
-    })
-  end
+    def editing(data)
+      return unless @root && current_user
 
-  def stopped_editing(data)
-    return unless @root && current_user
+      creative_id = data["creative_id"].to_i
+      Rails.logger.info "[CreativesChannel] User #{current_user.email} editing creative##{creative_id}"
+      CreativesChannel.broadcast_to(@root, {
+        editing: {
+          creative_id: creative_id,
+          user_id: current_user.id,
+          user_name: current_user.display_name
+        }
+      })
+    end
 
-    creative_id = data["creative_id"].to_i
-    Rails.logger.info "[CreativesChannel] User #{current_user.email} stopped editing creative##{creative_id}"
-    CreativesChannel.broadcast_to(@root, {
-      stopped_editing: {
-        creative_id: creative_id,
-        user_id: current_user.id
-      }
-    })
-  end
+    def stopped_editing(data)
+      return unless @root && current_user
 
-  private
+      creative_id = data["creative_id"].to_i
+      Rails.logger.info "[CreativesChannel] User #{current_user.email} stopped editing creative##{creative_id}"
+      CreativesChannel.broadcast_to(@root, {
+        stopped_editing: {
+          creative_id: creative_id,
+          user_id: current_user.id
+        }
+      })
+    end
 
-  def broadcast_presence
-    user_ids = Collavre::CreativePresenceStore.list(@root.id)
-    CreativesChannel.broadcast_to(@root, { presence: { user_ids: user_ids } })
+    private
+
+    def broadcast_presence
+      user_ids = CreativePresenceStore.list(@root.id)
+      CreativesChannel.broadcast_to(@root, { presence: { user_ids: user_ids } })
+    end
   end
 end
