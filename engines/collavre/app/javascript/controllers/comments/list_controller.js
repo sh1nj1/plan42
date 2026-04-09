475904316813da28a312110628e2d4bdd69794df
Soonoh <soonoh.jung@gmail.com>
2026-04-09 15:03:09 +0900

    refactor: deduplicate select-all logic in list controller
    Replace inline select/deselect logic in the Select All checkbox handler
    with calls to existing selectAll() and clearSelection() methods.

--- engines/collavre/app/javascript/controllers/comments/list_controller.js
+++ engines/collavre/app/javascript/controllers/comments/list_controller.js
@@ -563,28 +563,11 @@ export default class extends Controller {
 
     // Select All toggle handler
     selectAllCheckbox.addEventListener('change', () => {
-      const shouldSelect = selectAllCheckbox.checked
-      this.listTarget.querySelectorAll('.comment-select-checkbox').forEach((checkbox) => {
-        const commentId = checkbox.value
-        const item = checkbox.closest('.comment-item')
-        if (shouldSelect && !checkbox.checked) {
-          checkbox.checked = true
-          this.selection.add(commentId)
-          if (item) {
-            item.classList.add('selected-for-move')
-            item.setAttribute('draggable', 'true')
-          }
-        } else if (!shouldSelect && checkbox.checked) {
-          checkbox.checked = false
-          this.selection.delete(commentId)
-          if (item) {
-            item.classList.remove('selected-for-move')
-            item.removeAttribute('draggable')
-          }
-        }
-      })
-      this.notifySelectionChange()
-      this.updateSelectionActionBar()
+      if (selectAllCheckbox.checked) {
+        this.selectAll()
+      } else {
+        this.clearSelection()
+      }
     })
 
     bar.querySelector('.selection-action-delete').addEventListener('click', (e) => { e.stopPropagation(); this.deleteSelectedComments() })
