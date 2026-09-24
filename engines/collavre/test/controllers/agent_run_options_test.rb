9ea94b7e519ab98cf22e79de3cbc59fee1b17742
Soonoh <soonoh.jung@gmail.com>
2026-09-24 19:51:27 +0900

    style: use brain icon for chat thinking settings

--- engines/collavre/test/controllers/agent_run_options_test.rb
+++ engines/collavre/test/controllers/agent_run_options_test.rb
@@ -72,7 +72,10 @@ class AgentRunOptionsControllersTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "form#new-comment-form[data-controller~='comments--run-options']" do
-      assert_select "button.agent-run-options-toggle[aria-controls='agent-run-options-panel']"
+      assert_select "button.agent-run-options-toggle[aria-controls='agent-run-options-panel']" do
+        assert_select "[aria-label=?]", I18n.t("collavre.comments.agent_run_options.button_title")
+        assert_select "svg.thinking-icon[aria-hidden='true'][focusable='false']"
+      end
       assert_select "select[hidden][name='comment[agent_run_options][reasoning_effort]'] option[value='xhigh']"
       assert_select "input[name='comment[agent_run_options][model]']", count: 0
       assert_select "datalist#agent-run-options-models", count: 0
